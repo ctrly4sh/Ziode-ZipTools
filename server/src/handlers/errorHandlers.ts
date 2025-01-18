dd4b9231eeb84c1a9f75a748fce783ee3a838e63
ctrly4sh <149458186+ctrly4sh@users.noreply.github.com>
2025-01-19 00:02:28 +0530

    cleared things out :)

--- server/src/handlers/errorHandlers.ts
+++
@@ -1,5 +0,0 @@
-import * as winston from "winston";
-
-const file = new winston.transports.File({
-    
-})
--- server/src/temp-logs.js
+++
@@ -1,4 +0,0 @@
-const path = require("path");
-const sessionID = `Session-${Date.now()}`;
-const tempDirectory = path.join(__dirname, 'uploads', sessionID);
-console.log(tempDirectory)
